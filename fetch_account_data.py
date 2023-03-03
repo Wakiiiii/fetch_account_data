d055bc76a5dabdbc851af2ce746ddd86a6f4bc5f
waki <theointernaute@gmaile.com>
2023-03-03 19:41:19 +0700

    Add keys input fields

--- fetch_account_data.py
+++ fetch_account_data.py
@@ -17,9 +17,13 @@ window.resizable(0, 0)
 # Add GUI elements
 api_key_label = tk.Label(window, text='API Key:')
 api_key_label.grid(row=0, column=0, padx=5, pady=5)
+api_key_line = tk.Entry(window)
+api_key_line.grid(row=0, column=1, padx=0, pady=0)
 
 secret_key_label = tk.Label(window, text='Secret Key:')
 secret_key_label.grid(row=1, column=0, padx=5, pady=5)
+secret_key_line = tk.Entry(window)
+secret_key_line.grid(row=1, column=1, padx=0, pady=0)
 
 json_file_label = tk.Label(window, text='JSON file:')
 json_file_label.grid(row=2, column=0, padx=5, pady=5)
